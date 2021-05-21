c3122ad592de3f71b03b83ca7ab76d3f90ab65f0
Alex-A-B <alexjamesbelton@gmail.com>
2021-05-21 10:45:24 +0100

    starting on like event listener

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ const FULL_HEART = '♥'
 
 // HTML DOM variables
 const modal = document.getElementById("modal");
-
+const heart = document.querySelectorAll("like");
 
 
 // eventListeners
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    modal.hidden = true;
+    modal.className = "hidden";
 });
+
+
+
 
 
 
@@ -33,3 +36,21 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
+
+
+heart.forEach( item => {
+  item.addEventListener("click", (e) => {
+    mimicServerCall(); // will need to move function to top so it can be located by the event listener (or move below)
+    if (response == reject) {
+        modal.className = "";
+        setTimeout(function() {
+          modal.className = "hidden"
+        }, 3000);
+    } else {
+      e.target.innerText = `${FULL_HEART}`;
+      e.target.className = "activated-heart";
+    }  
+  })
+  });
